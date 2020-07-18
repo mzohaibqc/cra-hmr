ba6779509564c408d1bf32872dd2e0a222a05175
Zohaib Ijaz <mzohaib.qc@gmail.com>
2020-07-18 20:37:42 +0500

    Fixed linting issues. Now .eslintrc.js file is being used

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CRA-HMR
 
-This repository provides create-react-app based react boilerplate with Hot Module Repalcement implemented.
+This repository provides create-react-app based react boilerplate with Hot Module Replacement implemented.
 Code changes are reflected instantly in browser and state is maintained which helps a lot in development.
 
--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,46 @@
-const { override, addBabelPlugin, addWebpackAlias, useEslintRc, useBabelRc } = require('customize-cra');
+/* eslint-disable react-hooks/rules-of-hooks */
+const { override, addBabelPlugin, addWebpackAlias, useBabelRc } = require('customize-cra');
 
-module.exports = override(
-  addBabelPlugin('react-hot-loader/babel'),
-  addWebpackAlias({
-    'react-dom':  process.env.NODE_ENV === 'production' ? 'react-dom' : '@hot-loader/react-dom',
-  }),
-  useBabelRc(),
-  useEslintRc()
-)
+// Import your eslint configuration here
+const eslintConfig = require('./.eslintrc.js');
+
+const useEslintConfig = (configRules) => (config) => {
+  const updatedRules = config.module.rules.map((rule) => {
+    // Only target rules that have defined a `useEslintrc` parameter in their options
+    if (rule.use && rule.use.some((use) => use.options && use.options.useEslintrc !== void 0)) {
+      const ruleUse = rule.use[0];
+      const baseOptions = ruleUse.options;
+      const baseConfig = baseOptions.baseConfig || {};
+      const newOptions = {
+        useEslintrc: false,
+        ignore: true,
+        baseConfig: { ...baseConfig, ...configRules },
+      };
+      ruleUse.options = newOptions;
+      return rule;
+
+      // Rule not using eslint. Do not modify.
+    } else {
+      return rule;
+    }
+  });
+
+  config.module.rules = updatedRules;
+  return config;
+};
+
+// Support environment -specific settings
+const env = process.env.NODE_ENV;
+const envs = {
+  development: override(
+    addBabelPlugin('react-hot-loader/babel'),
+    addWebpackAlias({
+      'react-dom': process.env.NODE_ENV === 'production' ? 'react-dom' : '@hot-loader/react-dom',
+    }),
+    useEslintConfig(eslintConfig), // Use your imported .eslintrc.js file here
+    useBabelRc(),
+  ),
+  production: override(useBabelRc()),
+};
+
+module.exports = envs[env];
